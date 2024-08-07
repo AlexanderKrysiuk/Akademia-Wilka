dea22e55b6c23857063cedec196493b09acf461a
Alexander Krysiuk <alex.krysiuk@icloud.com>
2024-08-07 11:28:15 +0200

    logo as link to main page

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 const Header = () => {
     return ( 
         <div className="h-[10vh] w-full border-4 border-red-500 items-center flex px-[1vw] py-[1vh] justify-between">
-            <div className='flex items-center space-x-[1vh]'>
+            <Link href="/" passHref className='flex items-center space-x-[1vh]'>    
                 <div className="relative flex w-[8vh] h-[8vh]">
                     <Image
                         src="/logo.png"
@@ -16,9 +17,9 @@ const Header = () => {
                 <h1>
                     Akademia Wilka
                 </h1>
-            </div>
+            </Link>
             <div>
-                <Button>
+                <Button className='max-h-[8vh]'>
                     Zacznij Tutaj
                 </Button>
             </div>

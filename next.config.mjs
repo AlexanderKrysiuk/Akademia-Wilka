276d70dce091f295f812c3101c4e18b44d8d35eb
Alexander Krysiuk <alex.krysiuk@icloud.com>
2024-10-13 22:00:57 +0200

    changed to production

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 import autoCert from "anchor-pki/auto-cert/integrations/next";
 
 const withAutoCert = autoCert({
-  enabledEnv: "development",
+  enabledEnv: "production",
 });
 
 const nextConfig = {

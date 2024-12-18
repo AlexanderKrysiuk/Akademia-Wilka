ae494bac7942c3bb7eeb72beda6b5406ab8cd3b9
Alexander Krysiuk <alex.krysiuk@icloud.com>
2024-12-18 22:41:13 +0100

    uninstall cert

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
-import autoCert from "anchor-pki/auto-cert/integrations/next";
-
-const withAutoCert = autoCert({
-  enabledEnv: process.env.NODE_ENV,
-});
-
 const nextConfig = {
   images: {
     domains: ['akademiawilka.pl', 'wolfpath.atthost24.pl'], // Obie domeny, z których pobierane są obrazy
   },
 };
 
-export default withAutoCert(nextConfig);
+export default nextConfig;

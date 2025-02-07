90d6ded1592badc845568863db34a885aa7938ad
Alexander Krysiuk <alex.krysiuk@icloud.com>
2025-02-07 14:06:06 +0100

    Polityka Prywatności

---
+++ app/polityka-prywatnosci/page.tsx
@@ -0,0 +1,92 @@
+const Regulations = () => {
+    return ( 
+        <main>
+            <h1>Polityka Prywatności i Regulamin Korzystania z Usług</h1>
+            <p><strong>Data ostatniej aktualizacji:</strong> 22-02-2025</p>
+
+            <h2>1. Polityka Prywatności</h2>
+
+            <h3>1.1. Jakie dane osobowe zbieramy?</h3>
+            <p>Zbieramy dane osobowe, które użytkownicy dobrowolnie przekazują podczas korzystania z naszych usług, takie jak:</p>
+            <ul>
+                <li>Imię i nazwisko</li>
+                <li>Adres e-mail</li>
+                <li>Dane do płatności (np. numer karty kredytowej)</li>
+                <li>Dane o aktywności na naszej platformie (np. postępy w kursach, historia zakupów)</li>
+            </ul>
+
+            <h3>1.2. Cel przetwarzania danych osobowych</h3>
+            <p>Twoje dane osobowe są przetwarzane w celu:</p>
+            <ul>
+                <li>Realizacji zamówień i płatności</li>
+                <li>Obsługi konta użytkownika (rejestracja, logowanie, zarządzanie subskrypcjami)</li>
+                <li>Ulepszania naszych usług oraz dostosowania ich do Twoich potrzeb</li>
+                <li>Kontaktowania się z użytkownikami w sprawach związanych z usługami</li>
+            </ul>
+
+            <h3>1.3. Jak długo przechowujemy dane?</h3>
+            <p>Dane osobowe przechowujemy przez czas niezbędny do realizacji usług oraz zgodnie z przepisami prawa (np. dla celów podatkowych czy rozliczeniowych).</p>
+
+            <h3>1.4. Prawa użytkowników</h3>
+            <p>Masz prawo do:</p>
+            <ul>
+                <li>Dostępu do swoich danych osobowych</li>
+                <li>Poprawiania, usuwania lub ograniczania przetwarzania danych</li>
+                <li>Wniesienia skargi do organu nadzorczego, jeśli uważasz, że Twoje dane są przetwarzane niezgodnie z prawem</li>
+            </ul>
+
+            <h3>1.5. Cookies</h3>
+            <p>Nasza strona internetowa używa plików cookies, aby poprawić funkcjonalność strony, analizować ruch na stronie oraz personalizować doświadczenia użytkownika. Możesz zarządzać ustawieniami cookies w swojej przeglądarce.</p>
+
+            <h3>1.6. Bezpieczeństwo danych</h3>
+            <p>Stosujemy odpowiednie środki techniczne i organizacyjne w celu ochrony Twoich danych osobowych przed nieautoryzowanym dostępem, utratą lub usunięciem.</p>
+
+            <h2>2. Regulamin Korzystania z Usług</h2>
+
+            <h3>2.1. Definicje</h3>
+            <ul>
+                <li><strong>Usługa</strong>: Kursy, szkolenia, sesje oraz inne produkty oferowane na naszej platformie.</li>
+                <li><strong>Użytkownik</strong>: Osoba, która korzysta z naszych usług, rejestrując konto i dokonując płatności.</li>
+                <li><strong>Administrator</strong>: Operator platformy, oferujący usługi i zapewniający obsługę klienta.</li>
+            </ul>
+
+            <h3>2.2. Rejestracja i konto użytkownika</h3>
+            <p>Aby korzystać z naszych usług, użytkownik musi zarejestrować konto na naszej platformie, podając wymagane dane osobowe (np. e-mail, imię, nazwisko). Jeżeli użytkownik nie posiada konta, zostanie ono automatycznie utworzone podczas pierwszego zakupu.</p>
+
+            <h3>2.3. Zasady korzystania z usług</h3>
+            <p>Usługi są dostępne dla użytkowników, którzy dokonali rejestracji na platformie. Użytkownicy zobowiązani są do przestrzegania regulaminu oraz zasad korzystania z usług, w tym do nieudostępniania swojego konta osobom trzecim. W przypadku naruszenia regulaminu, administrator ma prawo do zablokowania dostępu do konta użytkownika.</p>
+
+            <h3>2.4. Zakupy i płatności</h3>
+            <p>Produkty na naszej platformie są oferowane w określonej cenie. Ceny są wyrażone w złotówkach i są płatne z góry. Za pomocą naszej platformy można dokonywać płatności za pomocą dostępnych metod płatności (np. karta kredytowa, przelew bankowy). Płatności obsługiwane są przez zewnętrznego dostawcę (np. Stripe), który zapewnia bezpieczne przetwarzanie transakcji.</p>
+
+            <h3>2.5. Zasady zwrotów i reklamacji</h3>
+            <p>Użytkownik ma prawo do zwrotu zakupionych kursów w terminie 14 dni od daty zakupu, pod warunkiem, że nie rozpoczął korzystania z kursu. W przypadku reklamacji, użytkownik może zgłosić problem do działu obsługi klienta, a reklamacja zostanie rozpatrzona zgodnie z obowiązującymi przepisami prawa.</p>
+
+            <h3>2.6. Odpowiedzialność</h3>
+            <p>Usługodawca nie ponosi odpowiedzialności za szkody wynikłe z niewłaściwego korzystania z platformy, w tym za nieuprawniony dostęp do konta użytkownika. Użytkownik odpowiada za swoje dane logowania oraz bezpieczeństwo swojego konta.</p>
+
+            <h3>2.7. Zmiany w regulaminie i polityce prywatności</h3>
+            <p>Zastrzegamy sobie prawo do zmiany regulaminu oraz polityki prywatności. Wszelkie zmiany będą publikowane na tej stronie i wejdą w życie w terminie wskazanym w komunikacie.</p>
+
+            <h2>3. Postanowienia końcowe</h2>
+
+            <h3>3.1. Kontakt</h3>
+            <p>W przypadku pytań dotyczących regulaminu lub polityki prywatności, prosimy o kontakt na adres e-mail: alex.krysiuk@icloud.com</p>
+
+            <h3>3.2. Prawo właściwe</h3>
+            <p>Niniejszy regulamin oraz polityka prywatności podlegają prawu Polskiemu, a wszelkie spory będą rozstrzygane przez sądy właściwe dla siedziby usługodawcy.</p>
+
+            <h2>4. Ochrona praw autorskich i logo</h2>
+            <h3>4.1. Ochrona materiałów</h3>
+            <p>Materiały dostępne na stronie Akademii Wilka (w tym kursy, szkolenia, teksty, obrazy, wideo i inne treści) są chronione prawami autorskimi i stanowią własność Akademii Wilka. Zabrania się kopiowania, rozpowszechniania, modyfikowania, publikowania lub wykorzystywania tych materiałów w jakiejkolwiek formie bez pisemnej zgody właściciela praw autorskich. Wszystkie prawa do materiałów są zastrzeżone.</p>
+
+            <h3>4.2. Ochrona logo i znaków towarowych</h3>
+            <p>Logo Akademii Wilka oraz inne znaki towarowe używane na stronie są własnością Akademii Wilka i nie mogą być wykorzystywane bez jej wyraźnej pisemnej zgody.</p>
+
+            <h2>Zgoda na regulamin i politykę prywatności</h2>
+            <p>Korzystając z naszych usług, użytkownik akceptuje powyższy regulamin oraz politykę prywatności. Użytkownik oświadcza, że zapoznał się z treścią dokumentu i zgadza się na przetwarzanie swoich danych osobowych zgodnie z zawartymi w nim zasadami.</p>
+        </main>
+    );
+}
+
+export default Regulations;
